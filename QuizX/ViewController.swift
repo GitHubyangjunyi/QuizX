778f39789f5f9e9e89b5ce56b55325bfb75ca8b5
杨俊艺 <1209032479@qq.com>
2019-12-15 19:58:24 +0800

    完成使用约束实现飞入飞出动画

--- QuizX/ViewController.swift
+++ QuizX/ViewController.swift
@@ -13,10 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         currentQuestionLabel.text = questions[currentQuestionIndex]
+        updateOffScreenLabel()
+    }
+    //视图将要显示出来时
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        nextQuestionLabel.alpha = 0//下一个问题消失透明度为0表示消失
     }
     
     @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var answerLabel: UILabel!
     
     @IBAction func showNextQuestion(sender: AnyObject)
@@ -25,9 +33,9 @@ class ViewController: UIViewController {
         if currentQuestionIndex == questions.count {
             currentQuestionIndex = 0
         }
-        nextQuestionLabel.text = questions[currentQuestionIndex]
+        nextQuestionLabel.text = questions[currentQuestionIndex]//使用下一个问题标签显示问题
         answerLabel.text = "???"
-        animateLabelTransitions()
+        animateLabelTransitions()//执行动画
     }
         
     @IBAction func showAnswer(sender: AnyObject)
@@ -46,16 +54,26 @@ class ViewController: UIViewController {
                             ]
 
     func animateLabelTransitions() {
-        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
-            self.currentQuestionLabel.alpha = 0
-            self.nextQuestionLabel.alpha = 1
+        self.view.layoutIfNeeded()//强制更新布局防止答案标签也参与动画,也就是让视图在动画前布局好再开始动画
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0//让下一个问题回到屏幕中间
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth//让当前问题飞到屏幕右侧之外
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [.curveLinear], animations: {
+            self.currentQuestionLabel.alpha = 0//当前问题标签消失
+            self.nextQuestionLabel.alpha = 1//下一个问题标签显示
+            
+            self.view.layoutIfNeeded()
         }, completion: {_ in
-            swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+            swap(&self.currentQuestionLabel, &self.nextQuestionLabel)//交换两个标签
+            swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)//交换两个约束
+            self.updateOffScreenLabel()
         })
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        nextQuestionLabel.alpha = 0
+    //让下一个问题标签位于屏幕左侧之外
+    func updateOffScreenLabel() {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
     }
 }
