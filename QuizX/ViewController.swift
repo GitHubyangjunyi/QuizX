d8ce7f7b16767af668b800c22e9acf985e90ed85
杨俊艺 <1209032479@qq.com>
2019-12-15 16:17:56 +0800

    Initial Commit

---
+++ QuizX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizX
+//
+//  Created by 杨俊艺 on 2019/12/15.
+//  Copyright © 2019 杨俊艺. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
